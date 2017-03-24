843b729c901149eaf9ca8141dfacf389ba6a5bdc
Katchau <joaohccl@live.com.pt>
2017-03-24 16:15:20 +0000

    Lil update
    nid tu start verifying type

--- src/comp2/Teste.java
+++ src/comp2/Teste.java
@@ -1,8 +1,12 @@
 package comp2;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.DefaultGraph;
 import org.graphstream.stream.file.FileSource;
 import org.graphstream.stream.file.FileSourceFactory;
@@ -19,11 +23,24 @@ public class Teste {
 			
 			fs.begin(file);
 			while(fs.nextEvents()){
-				g.display();
+				
 			}
 			
 		} catch (IOException e) {
 			System.err.println("Error: No such file" + file);
+		}
+		
+		g.display();
+//		ArrayList<String> states = new ArrayList<String>();
+		
+		for(int i = 0; i < g.getNodeCount(); i++){
+			Node n = g.getNode(i);
+			System.out.println("Node "+ i + " : " + g.getNode(i).getId());
+			Iterator<Edge> edges = n.getEdgeSet().iterator();
+			while(edges.hasNext()){
+				Edge e = edges.next();
+				System.out.println("Edge " + e.getAttribute("label"));
+			}
 		}
 		
 		//ter isto separado porcausa da biblioteca. ver http://graphstream-project.org/doc/Tutorials/Reading-files-using-FileSource/ 
