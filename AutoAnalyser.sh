7b32589bb1c6c0e0aa58c77ea61c9411b9c56577
rodavoce <up201404178@fe.up.pt>
2017-04-27 21:40:34 +0100

    reorganize project structure

--- AutoAnalyser.sh
+++ AutoAnalyser.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+rm -r ./bin
+mkdir ./bin
+cp Input.aa bin/Input.aa
+cd src/
 jjtree AutoAnalyserParser.jjt        
-javacc AutoAnalyserParser.jj
+javacc.sh AutoAnalyserParser.jj
 javac *.java
+mv *.class ../bin/
+cd ../bin/
 java AutoAnalyser
+
