9af358a2fca2f1866bf055e1806def533998d86a
rodavoce <up201404178@fe.up.pt>
2017-06-05 18:11:19 +0100

    linked front and back end

--- src/AutoAnalyser.java
+++ src/AutoAnalyser.java
@@ -1,22 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
 import java.io.*;
 
 public class AutoAnalyser{
 
+      private Gui gui;
+      private AutoAnalyserParser analyserParser;
+
+      public static String runResult = new String();
       public static void main(String args[]) throws ParseException, FileNotFoundException {
-        File f = new File("Input2.aa");
-        FileReader fr = new FileReader(f);
-        BufferedReader br = new BufferedReader(fr);
-        AutoAnalyserParser analyser = new AutoAnalyserParser(br);
+            AutoAnalyser auto = new AutoAnalyser();
+      }
 
-        SimpleNode start;
+      public AutoAnalyser() {
+          EventQueue.invokeLater(() -> {
+              gui = new Gui(this);
+              gui.setVisible(true);
+              gui.setExtendedState(gui.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+          });
+        }
 
-          //try {
-               start = analyser.Start();
-              //System.out.println("accepted");
-               start.dump("");
-          //} catch (Exception t){
-              //System.out.println("rejected");
-          //}
-              start.execute();
+        public String run(File cache) {
+            runResult = "";
+            try {
+                FileReader fr = new FileReader(cache);
+                BufferedReader br = new BufferedReader(fr);
+                if(analyserParser == null)
+                    analyserParser = new AutoAnalyserParser(br);
+                else {
+                    analyserParser.ReInit(fr);
+                }
+                SimpleNode start = analyserParser.Start();
+                start.execute();
+                br.close();
+                fr.close();
+            } catch (Exception e) {
+                addToResult(e.getMessage());
+            }
+            return runResult;
+        }
+
+        public static void addToResult(String s) {
+          runResult += runResult + "\n" + s;
         }
 }
